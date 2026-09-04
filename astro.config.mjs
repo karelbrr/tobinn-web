c2b0ebd118446f7fca017441acff529bda3bc8a7
Karel Braborec <karelbr7@gmail.com>
2026-09-04 09:56:10 +0200

    feat: implement contact form, add sitemap, robots.txt, and 404 page, and refine UI components

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc()],
+  site: 'https://tobinn.cz',
+  integrations: [react(), markdoc(), sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
